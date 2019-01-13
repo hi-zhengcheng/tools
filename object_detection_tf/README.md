dbaf1d63c7627b630f764713880fde5fda028f5c
zhengcheng <hi_zhengcheng@163.com>
2019-01-12 21:11:37 -0800

    update

--- object_detection_tf/README.md
+++ object_detection_tf/README.md
@@ -2,7 +2,7 @@
 
 This demo uses tensorflow hub to build a simple realtime object detection application.
 
-### Install TensorFlow hub
+### 1. Install TensorFlow hub
 
 Follow instructions from [here](https://www.tensorflow.org/hub/installation):
 
@@ -10,8 +10,13 @@ Follow instructions from [here](https://www.tensorflow.org/hub/installation):
 pip install "tensorflow>=1.7.0"
 pip install tensorflow-hub
 ```
+
+### 2. Prepare Model
+`model.py` includes all things to init the model.
+
+**Note**: only batch_size 1 supported.
     
-### TensorFlow hub module cache
+### 3. TensorFlow hub module cache
 
 By default, the pretrained model will be downloaded and saved into one temp dir. You can specify other dir as follows:
 
@@ -22,11 +27,11 @@ os.environ['TFHUB_CACHE_DIR'] = 'your_module_cache_dir'
 
 Make sure set this env before any tf hub calls.
 
-### Single Image object detection
+### 4. Single Image object detection
 
 `infer_image.py` demos how to do object detection on single image. 
 You can change top_k and colors to display more results.
 
-### Realtime Camera object detection
+### 5. Realtime Camera object detection
 
 `infer_real_time_camera.py` demos how to do object detection on real time video.
--- object_detection_tf/model.py
+++ object_detection_tf/model.py
@@ -3,8 +3,12 @@ import tensorflow as tf
 
 
 class Model(object):
-    """"""
     def __init__(self):
+        """Init model, do things like:
+            1. Create input placeholder
+            2. load pre-trained model
+            3. extract interested tensor out from the model
+        """
         self.input_image = tf.placeholder(tf.float32)
         module = hub.Module('https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1')
         detector = module(self.input_image, as_dict=True)
@@ -12,6 +16,19 @@ class Model(object):
         self.boxes = detector['detection_boxes']
 
     def __call__(self, sess, batch_images, top_k):
+        """
+        Args:
+            sess: tf.Session obj
+            batch_images: numpy array with shape (1, height, width, channel)
+            top_k: integer, only return top_k result
+
+        Returns:
+            names: top_k class names sorted by score, with shape (top_k)
+            boxes: top_k bounding boxes sorted by score, with shape (top_k, 4).
+                Last dim data means (h1, w1, h2, w2)
+
+        Note: This pre-trained model only support batch_size 1. It will do squeeze dim[0] when model returns output.
+        """
         names, boxes = sess.run([self.class_names, self.boxes], feed_dict={self.input_image: batch_images})
         names = names[:top_k]
         boxes = boxes[:top_k]
